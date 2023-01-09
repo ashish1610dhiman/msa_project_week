5818ed45a1d9d7f848bb19de49b3e00b720b7b76
ASHISH DHIMAN <ashish1610dhiman@gmail.com>
2023-01-09 15:40:08 -0500

    hmm v1 results sku level

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ Contact at ashish1610dhiman@gmail.com
 """
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def rmse(y_act,y_pred):
     """
@@ -11,9 +12,10 @@ def rmse(y_act,y_pred):
     y_pred : pd df with Encoded_SKU_ID and SALES_DATE
     return --> rmse agg
     """
-    assert all(y_act.dtypes.values == y_pred.dtypes.values), "Column types do not match"
+    assert all(y_act[["SALES_DATE"]].dtypes.values == y_pred[["SALES_DATE"]].dtypes.values \
+               ), "Column types do not match"
     df_join = y_act.merge(y_pred,on=["Encoded_SKU_ID","SALES_DATE"])
-    assert df_join.shape[0]==y_act.shape[0], "y_act and y_pred shapes do not match"
+    # assert df_join.shape[0]==y_act.shape[0], "y_act and y_pred shapes do not match"
     rmse = np.sqrt(np.mean((df_join.actual-df_join.predicted)**2))
     return rmse
 
@@ -23,9 +25,10 @@ def rmse_sku(y_act,y_pred, asc_sort = False):
     y_pred : pd df with Encoded_SKU_ID and SALES_DATE
     return --> rmse for each sku
     """
-    assert all(y_act.dtypes.values == y_pred.dtypes.values), "Column types do not match"
+    assert all(y_act[["SALES_DATE"]].dtypes.values == y_pred[["SALES_DATE"]].dtypes.values\
+               ), "Column types do not match"
     df_join = y_act.merge(y_pred,on=["Encoded_SKU_ID","SALES_DATE"])
-    assert df_join.shape[0]==y_act.shape[0], "y_act and y_pred shapes do not match"
+    # assert df_join.shape[1]==y_act.shape[1], "y_act and y_pred shapes do not match"
     rmse_lambda = lambda x: np.sqrt(np.mean((x.actual-x.predicted)**2))
     rmse_skus = df_join.groupby("Encoded_SKU_ID").apply(lambda x: (rmse_lambda(x),x["actual"].mean()))
     rmse_skus1 = pd.DataFrame(rmse_skus.tolist(), index=rmse_skus.index)
@@ -33,22 +36,31 @@ def rmse_sku(y_act,y_pred, asc_sort = False):
     rmse_skus1["pct_rmse"] = rmse_skus1["rmse_du"]/rmse_skus1["mean_du"]
     return rmse_skus1.sort_values(by=["rmse_du"],ascending = asc_sort)
 
-def plot_pred_sku(train,y_act,y_pred, sku_id, start_dt = "2022-07-01"):
+def plot_pred_sku(train,y_act,y_pred, sku_id, start_dt = "2022-07-01", ax1 = None):
     """
     y_act : pd df with Encoded_SKU_ID and SALES_DATE
     y_pred : pd df with Encoded_SKU_ID and SALES_DATE
     return --> plot of actual vs predicted
     """
-    assert all(y_act.dtypes.values == y_pred.dtypes.values), "Column types do not match"
+    assert all(y_act[["SALES_DATE"]].dtypes.values == y_pred[["SALES_DATE"]].dtypes.values \
+               ), "Column types do not match"
     df_join = y_act.merge(y_pred,on=["Encoded_SKU_ID","SALES_DATE"])
-    assert df_join.shape[0]==y_act.shape[0], "y_act and y_pred shapes do not match"
+    # assert df_join.shape[0]==y_act.shape[0], "y_act and y_pred shapes do not match"
     train_sku = train[train.Encoded_SKU_ID==sku_id][["Encoded_SKU_ID","SALES_DATE","DAILY_UNITS"\
                                                 ]].rename(columns={"DAILY_UNITS":'actual'})
     train_valid_sku = pd.concat([train_sku,y_act[y_act.Encoded_SKU_ID==sku_id]])
     y_pred_sku = y_pred[y_pred.Encoded_SKU_ID==sku_id]
     df_join = train_valid_sku.merge(y_pred_sku,on=["SALES_DATE"], how ="outer")
-    df_join[df_join.SALES_DATE>=start_dt].plot(x="SALES_DATE",y=["actual","predicted"],color=["black","r"])
-    plt.axvspan(y_pred_sku["SALES_DATE"].min(), y_pred_sku["SALES_DATE"].max(),\
+    df_join_subset = df_join[df_join.SALES_DATE>=start_dt]
+    df_join_subset.plot(ax = ax1,x="SALES_DATE",y=["actual","predicted"],color=["black","r"])
+    if ax1:
+        ad_plot = ax1
+        ad_plot.title.set_text(
+            f"Actual/Predicted sales of sku:{sku_id} from {df_join_subset.SALES_DATE.min():%Y-%m-%d} to {df_join_subset.SALES_DATE.max():%Y-%m-%d}")
+    else:
+        ad_plot = plt
+        plt.title(
+            f"Actual/Predicted sales of sku:{sku_id} from {df_join_subset.SALES_DATE.min():%Y-%m-%d} to {df_join_subset.SALES_DATE.max():%Y-%m-%d}")
+    ad_plot.axvspan(y_pred_sku["SALES_DATE"].min(), y_pred_sku["SALES_DATE"].max(),\
             facecolor='pink', alpha=0.34, label ="Prediction week")
-    plt.title (f"Actual/Predicted sales of sku:{sku_id} from {train_valid_sku.SALES_DATE.min():%Y-%m-%d} to {y_pred_sku.SALES_DATE.max():%Y-%m-%d}")
-    plt.legend()
+    ad_plot.legend()
